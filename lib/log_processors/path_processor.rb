f55a6eb49666ee74794dacc6c7b2ad112d3467f3
Christopher Bradbury <cbradbury@peopleadmin.com>
2013-12-03 21:38:11 -0600

    round of path processing.  to use:
    require "#{Rails.root}/lib/log_processors/path_processor"
    include PathProcessor
    process_paths

---
+++ lib/log_processors/path_processor.rb
@@ -0,0 +1,38 @@
+module PathProcessor
+
+  def process_paths
+    format_log_paths
+    format_latest_routes_paths
+  end
+
+  def format_log_paths
+    StartedLine.all.each do |line|
+      formatted_path = line.path
+      # replace id with ":id"
+      formatted_path.gsub!(/\d+/, ":id")
+      # delete all query info, from "?" forward
+      formatted_path.gsub!(/\?.*/, ":id")
+      line.update_attributes(formatted_path: formatted_path)
+    end
+  end
+
+  def format_latest_routes_paths
+    routes_with_latest_import_timestamp.each do |line|
+      unless (line.name == "root" || line.name == "SKIPPED")
+        formatted_path = line.path
+        formatted_path.gsub!(/\/:(.*?)_id/, "/:id")
+        line.update_attributes(formatted_path: formatted_path)
+      end
+    end
+  end
+
+private
+
+  def routes_with_latest_import_timestamp
+    raise "routes not imported" if RoutesImport.count == 0
+    routes_imports = RoutesImport.where(import_timestamp: RoutesImport.last.import_timestamp)
+    ids = routes_imports.select(:id).map{|import_route| import_route.id}
+    routes = Route.where(routes_import_id: ids)
+    routes
+  end
+end
--- lib/tasks/routes_import.rake
+++ lib/tasks/routes_import.rake
@@ -4,10 +4,12 @@ require "#{Rails.root}/app/controllers/application_controller"
 require "#{Rails.root}/app/controllers/routes_import_controller"
 require "#{Rails.root}/lib/routes_importers/rails_route_import_support"
 require "#{Rails.root}/lib/routes_importers/rails_routes_import"
+require "#{Rails.root}/lib/log_processors/path_processor"
 require "#{Rails.root}/app/services/routes_import_service"
 require "#{Rails.root}/app/models/route"
 
 include RailsRouteImportSupport
+include PathProcessor
 
 namespace :import_routes do
   desc "Import routes from provided import_path or default path"
@@ -17,9 +19,10 @@ namespace :import_routes do
       RoutesImport.import(type: "rails", import_paths: ENV["IMPORT_PATHS"])
     else
       RoutesImport.import(type: "rails")
+      format_latest_routes_paths
     end
     end_count = Route.count
     added_count = end_count - start_count
-    puts "Imported #{added_count} routes; import_timestamp: #{RoutesImport.last.import_timestamp}"
+    puts "Processed #{added_count} routes; import_timestamp: #{RoutesImport.last.import_timestamp}"
   end
 end
