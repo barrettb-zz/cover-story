2e49774c87fcf56a71ea1341b76ba95ea7863430
Christopher Bradbury <cbradbury@peopleadmin.com>
2014-02-19 14:23:41 -0600

    checking in general progress/basic graphing,
    still workin progres

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,15 +1,11 @@
 class DashboardController < ApplicationController
   def index
+    @tested_results = Analysis.tested_results.to_json
 
-    tested_results = Analysis.tested_results
-    @tested_percentages = tested_results.map{|x| x[:percentage_covered]}
-    @tested_dates = tested_results.map{|x| x[:date].strftime("%Y/%m/%d")}
-
-# TODO this is not fully realized yet, demo only at this time
-    @tested_model_percentages = Analysis.tested_model_percentages
-
-# TODO determine if these need to go in view helpers, and also in config file
-    @tested_x_axis_label = APP_CONFIG[:analysis_config][:tested_x_axis_label]
-    @tested_y_axis_label = APP_CONFIG[:analysis_config][:tested_y_axis_label]
+    config = APP_CONFIG[:analysis_config]
+    @tested_labels = {
+      x_axis: config[:tested_x_axis_label],
+      y_axis: config[:tested_y_axis_label]
+    }.to_json
   end
 end
--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -4,19 +4,13 @@ class Analysis < ActiveRecord::Base
   has_many :analyzed_routes
   has_many :analyzed_route_models
 
-# TODO - does this need to convert to float? is this one even used?
-  # for now defining tested_percentages as anything we have collected over time
-  def self.tested_percentages
-    self.pluck(:percentage_covered).map(&:to_f)
-  end
-
-# TODO - does this need to convert to float?
   def self.tested_results
-    self.pluck(:percentage_covered, :created_at).map {|p, c| {percentage_covered: p.to_f, date: c.to_date}}
-  end
-
-# TODO this is not fully realized yet, demo only at this time
-  def self.tested_model_percentages
-    self.pluck(:model_percentage_covered).map(&:to_f)
+    self.pluck(:percentage_covered, :created_at, :model_percentage_covered).map {|p, c, m|
+      {
+        percentage_covered: p.to_f,
+        date: c,
+        model_percentage_covered: m.to_f
+      }
+    }
   end
 end
